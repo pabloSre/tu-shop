755b1305737a61699e70c65cf5d3554fe60a39db
Pablo Re <psre96@gmail.com>
2023-06-26 15:24:46 -0300

    funciones para user/botones signIn

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,23 +1,35 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { ShoppingCartContext } from "../../Context";
 import Layout from "../../Components/Layout";
 
 function SingIn() {
+  const context = useContext(ShoppingCartContext)
 
+  //Account
+  const account = localStorage.getItem('account')
+  const parsedAccount = JSON.parse(account)
+
+  // Has an account
+  const noAccountInLocalStorage = parsedAccount ? Object.keys(parsedAccount).length === 0 : true
+  const noAccountInLocalState = context.account ? Object.keys(context.account).length === 0 : true
+  const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalState
 return ( 
   <Layout>
     <h1 className='font-medium text-xl text-center mb-6 w-80'>Welcome</h1>
-  <div className='flex flex-col w-80'>
-    <p>
-      <span className='font-ligth text-sm'>Email:</span>
-      <span>Admin@admin.com</span>
-    </p>
+    <div className='flex flex-col w-80'>
+      <p>
+        <span className='font-ligth text-sm'>Email:</span>
+        <span>{parsedAccount?.email}</span>
+      </p>
     <p>
       <span className='font-light text-sm'>Password</span>
-      <span>*******</span>
+      <span>{parsedAccount?.password}</span>
     </p>
     <Link to='/'>
       <button
-      className='bg-black disabled:bg-black/40 text-white w-full rounded-lg py-3 mt-4 mb-2'>
+      className='bg-black disabled:bg-black/40 text-white w-full rounded-lg py-3 mt-4 mb-2'
+      disable={!hasUserAnAccount}>
         Log in
       </button>
     </Link>
@@ -25,7 +37,7 @@ return (
       <a className='font-light text-xs underline underline-offset-4' href='/'>forgot my password</a>
     </div>
     <button
-      className='border border-black disable:text-black/40 disable:border-black/40 rounded-lg mt-6 py-3'>
+      className='border border-black disable:text-black/40 disable:border-black/40 rounded-lg mt-6 py-3' disable={hasUserAnAccount}>
         Sign up
       </button>
       </div>
